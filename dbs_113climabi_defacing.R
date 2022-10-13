a1b04194b9615a276f0b127505efee039d0e3f08
josefmana <josef.mana@protonmail.com>
2022-10-13 21:37:49 +0200

    coregistration

--- dbs_113climabi_defacing.R
+++ dbs_113climabi_defacing.R
@@ -17,18 +17,18 @@ for ( i in pkgs ) {
 fls <- list.files( "data", recursive = T ) %>% as.data.frame() %>% slice( which( grepl("anat_t1",.) ) )
 
 # (manual step) keep only images with no defacing yet
-fls <- fls[ which(grepl("SUBJECT_078",fls$.)):nrow(fls), ]
-fls <- fls[ -which( grepl("SUBJECT_080",fls) ) ] # SUBJECT_080 is already defaced
+#fls <- fls[ which(grepl("SUBJECT_078",fls$.)):nrow(fls), ]
+#fls <- fls[ -which( grepl("SUBJECT_080",fls) ) ] # SUBJECT_080 is already defaced
 
 # rename the original file
-for ( i in fls ) file.rename( from = paste0("data/",i), to = paste0( "data/",gsub(".nii","_orig.nii",i) ) )
+for ( i in fls$. ) file.rename( from = paste0("data/",i), to = paste0( "data/",gsub(".nii","_orig.nii",i) ) )
   
 # write a .command script for fsl_deface
 writeLines(
   paste(
     paste( "fsl_deface", # call fsl_deface function
-           paste0( "~/Desktop/mercenaries/dbs_113climabi/data/", gsub(".nii","_orig.nii",fls) ), # original
-           paste0( "~/Desktop/mercenaries/dbs_113climabi/data/", fls ), # defaced
+           paste0( "~/Desktop/mercenaries/dbs_113climabi/data/", gsub(".nii","_orig.nii",fls$.) ), # original
+           paste0( "~/Desktop/mercenaries/dbs_113climabi/data/", fls$. ), # defaced
            sep = " " ),
     collapse = "\n"
   ), con = "conduct_fsl_deface.command"
@@ -40,12 +40,19 @@ system( "chmod u+x ~/Desktop/mercenaries/dbs_113climabi/conduct_fsl_deface.comma
 # now launch the "conduct_fsl_deface.command" file by double-clicking on it
 # the code will add a new defacedd "anat_t1.nii.gz" file to each subject's directory
 
-# unzip the defaced images
-for ( i in fls ) {
-  gunzip( paste0( getwd(), "/data/", i, ".gz") ) # unzipping
-  # remove the originaů
+# final touches
+for ( i in gsub("/.*","",fls$.) ) {
+  
+  # unzip the defaced images
+  gunzip( paste0( getwd(), "/data/", i, "/anat_t1.nii.gz") ) # unzipping
+  
+  # remove the original
   # make sure you have a reserve
-  file.remove( paste0( "~/Desktop/mercenaries/dbs_113climabi/data/", gsub(".nii","_orig.nii",fls) ) )
+  file.remove( paste0( "~/Desktop/mercenaries/dbs_113climabi/data/", i, "/anat_t1_orig.nii" ) )
+  
+  # remove coronal T2w images as they coregister poorly and are not needed
+  file.remove( paste0( "~/Desktop/mercenaries/dbs_113climabi/data/", i, "/anat_t2_cor.nii" ) )
+  
 }
 
 
